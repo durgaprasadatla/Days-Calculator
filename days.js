517abf96af10f5ddb85456a3d6127dff012d795e
dpatla95 <dpatla95@gmail.com>
2025-01-05 10:02:43 +0530

    added

---
+++ days.js
@@ -0,0 +1,9 @@
+var btn=document.getElementById("btn")  
+var input1=document.getElementById("input1")  
+var input2=document.getElementById("input2")  
+var answer=document.querySelector(".answer")  
+function ans(){  
+var Date1= new Date(input1.value)  
+var Date2= new Date(input2.value)  
+answer.innerHTML=`Difference between the 2 dates is : ${(Date1-Date2)/(1000*60*60*24)} days.`  
+}
